a47683ce994943bd640e8c38cd276591d1bd07bd
kksusu <31981518+kksusu@users.noreply.github.com>
2018-01-24 18:53:35 +0800

    Add files via upload

---
+++ b.java
@@ -0,0 +1,14 @@
+package first;
+
+public class b {
+	public static void main (String[] args){
+		int x=5;
+		while(x>1){
+			x=x-1;
+			if(x<3){
+				System.out.print("small x");
+			}
+		}
+	}
+
+}
---
+++ c.java
@@ -0,0 +1,21 @@
+package first;
+
+public class c {
+	public static void main(String[]args){
+		int x=0;
+		while (x<4){
+			System.out.print("a");
+			if(x<1){
+				System.out.print("noise");
+			}
+			x=x+2;
+			if(x>1){
+				System.out.print("annoys");
+				x=x+1;
+				
+			}
+			if()
+		}
+	}
+
+}
